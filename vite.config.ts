565f6ee66d553d97f9b4d3d11fb9b206d882d4f9
Tigran <tskanyan@nanosemantics.ai>
2024-01-24 20:23:57 +0300

    ipdated

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
-import { extname, relative, resolve } from "path";
-import { fileURLToPath } from "node:url";
-import { glob } from "glob";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), dts({ include: ["lib"] }), libInjectCss()],
   build: {
     copyPublicDir: false,
     lib: {
+      name: "react-hinter",
       entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
-      input: Object.fromEntries(
-        glob.sync("lib/**/*.{ts,tsx}").map((file) => [
-          // The name of the entry point
-          // lib/nested/foo.ts becomes nested/foo
-          relative("lib", file.slice(0, file.length - extname(file).length)),
-          // The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url)),
-        ]),
-      ),
+      external: ["react", "react-dom"],
       output: {
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
+        inlineDynamicImports: false,
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
       },
     },
   },
