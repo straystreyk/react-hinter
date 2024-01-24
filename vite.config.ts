d9e0054c1c510e1b3421d7c7a8c4c98ed6ab8dff
Tigran <tskanyan@nanosemantics.ai>
2024-01-24 22:41:41 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import { resolve } from "path";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), dts({ include: ["lib"] }), libInjectCss()],
   build: {
     copyPublicDir: false,
-    ssr: true,
-    minify: "esbuild",
     lib: {
       name: "react-hinter",
       entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["umd", "es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
-
+      external: ["react", "react-dom"],
       output: {
+        inlineDynamicImports: false,
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
